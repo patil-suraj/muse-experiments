4fa466657bb32118eb18c000fad72da13838a669
patil-suraj <surajp815@gmail.com>
2023-07-22 16:01:27 +0200

    fix

--- ctrlnet/train_controlnet_webdataset.py
+++ ctrlnet/train_controlnet_webdataset.py
@@ -173,7 +173,7 @@ class WebdatasetFilter:
         try:
             if "json" in x:
                 x_json = json.loads(x["json"])
-                filter_size = (x_json.get("original_width", 0.0) or 0.0) >= self.min_size or x_json.get(
+                filter_size = (x_json.get("original_width", 0.0) or 0.0) >= self.min_size and x_json.get(
                     "original_height", 0
                 ) >= self.min_size
                 filter_watermark = (x_json.get("pwatermark", 1.0) or 1.0) <= self.max_pwatermark
