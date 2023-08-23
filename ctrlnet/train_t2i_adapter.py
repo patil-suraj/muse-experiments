bc848ba81308afe559eafa9fc6d72dd38e4cddbe
patil-suraj <surajp815@gmail.com>
2023-08-23 18:10:30 +0200

    up

--- ctrlnet/train_t2i_adapter.py
+++ ctrlnet/train_t2i_adapter.py
@@ -1315,6 +1315,7 @@ def main(args):
                 timesteps = torch.rand((bsz, ), device=latents.device)
                 timesteps = (1 - timesteps**3) * noise_scheduler.config.num_train_timesteps
                 timesteps = timesteps.long().to(noise_scheduler.timesteps.dtype)
+                timesteps = timesteps.clamp(0, noise_scheduler.config.num_train_timesteps - 1)
 
                 # Add noise to the latents according to the noise magnitude at each timestep
                 # (this is the forward diffusion process)
